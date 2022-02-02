1d580143b58eec45fe157672a1607ee8d7da99cb
Joey-Flores <joeyflores98@gmail.com>
2022-02-02 11:43:28 -0800

    Changes made: game stops once a winner has reached 'Playing To' limit, and added color to scores

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const p1 = document.querySelector('#player1');
-const p2 = document.querySelector('#player2');
-
-const p1Point = document.querySelector('#green');
 
 let selection = document.querySelectorAll('option');
 
 let selection2 = document.querySelector('select');
 
 
-let selected = selection2.addEventListener('change', function() {
+let selected = selection2.addEventListener('change', function () {
     for (let i = 0; i < selection.length; i++) {
         if(selection[i].selected === true) {
             console.log(selection[i].value); //THIS IS TO TEST IF SELCETION IS LOCATED
@@ -17,41 +13,69 @@ let selected = selection2.addEventListener('change', function() {
     }
 })
 
-let p1Clicks = 0;
 
-
-p1Point.addEventListener('click', function() {
-    if (p1Clicks !== num){
-        p1Clicks += 1;
-        p1.innerHTML = p1Clicks;
-    }
-})
-
-while (p1Clicks !== 0) {
-        console.log('this works');
-}
-
-// if (p1Clicks !== 0 && p1Clicks === num) {
+// if (p1Clicks == num) {
 //         p1.style.color = 'green';
 //         console.log("green");
-//     }
+// }
 
-const p2Point = document.querySelector('#blue');
 
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const reset = document.querySelector('#reset');
+
+let p1Display = document.querySelector('#p1Display');
+let p2Display = document.querySelector('#p2Display');
+
+let p1Clicks = 0;
 let p2Clicks = 0;
 
-p2Point.addEventListener('click', function() {
-    if (p2Clicks !== num) {
-        p2Clicks += 1;
-        p2.innerHTML = p2Clicks
-    }
+let select = 0;
+
+let gameOver = false;
+
+p1Button.addEventListener('click', function () {
+    if (isNaN(num) === false) {
+        if(!gameOver) {
+            p1Clicks += 1;
+            if (p1Clicks === num) {
+                gameOver = true;
+            }
+            p1Display.textContent = p1Clicks;
+            }
+            if (p1Clicks === num) {
+                p1Display.classList.add('won');
+                p2Display.classList.add('lost');
+            }
+    }    
 })
 
-const reset = document.querySelector('#red');
+p2Button.addEventListener('click', function () {
+    if (isNaN(num) === false) {
+        if(!gameOver) {
+            p2Clicks += 1;
+            if (p2Clicks === num) {
+                gameOver = true;
+            }
+            p2Display.textContent = p2Clicks;
+            }
+            if (p2Clicks === num) {
+                p2Display.classList.add('won');
+                p1Display.classList.add('lost');
+            }
+    }
+})
 
 reset.addEventListener('click', function () {
     p1Clicks = 0;
     p2Clicks = 0;
-    p1.innerHTML = p1Clicks;
-    p2.innerHTML = p2Clicks;
+    gameOver = false;
+    p1Display.textContent = p1Clicks;
+    p2Display.textContent = p2Clicks;
+    p1Display.classList.remove('won', 'lost')
+    p2Display.classList.remove('won', 'lost')
 })
+
+if (isNaN(num) === false) {
+    console.log(1);
+}
